f7fc8921678cb8d794102fbdbdca0a732dddc9eb
Gennaro Rende <44288923+jackrende@users.noreply.github.com>
2019-05-20 13:20:09 +0200

    final update

--- Laboratory 1/Ex 1_4/Code to plot the fmax.m
+++ Laboratory 1/Ex 1_4/Code to plot the fmax.m
@@ -3,14 +3,14 @@ clear all; close all; clc;
 X=[4,6,8,10,12,14];
 
 Y_split=[40.6172, 40.5248, 39.5032, 39.7115, 40.0511, 38.7714];
-Y_no_split=[114.6550, 130.1530,];
+Y_no_split=[114.6550, 130.1530, 112.7120, 98.3395, 116.6230, 104.0330];
 
 
 figure(1);
 
 title('Objective funtions with different seeds');
 
-plot(X,Y_split,'-o',X,Y_no_split,'-o')
+semilogy(X,Y_split,'-o',X,Y_no_split,'-o')
 
 grid minor
 
